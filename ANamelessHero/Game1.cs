bd32579029bc982f47d5866e7ab1821b6b70db7c
Nanaka <Nanaka.kumiho@gmail.com>
2022-04-07 12:04:44 +0200

    Adding generalFunction

--- ANamelessHero/Game1.cs
+++ ANamelessHero/Game1.cs
@@ -18,8 +18,7 @@ namespace ANamelessHero
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
+            GeneralFunctions.Init(_graphics, Content, GraphicsDevice);
             base.Initialize();
         }
 
@@ -27,24 +26,20 @@ namespace ANamelessHero
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
+            GeneralFunctions.GameTime = gameTime;
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
-            // TODO: Add your update logic here
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
+            GraphicsDevice.Clear(Color.Black);
 
             base.Draw(gameTime);
         }
---
+++ ANamelessHero/Project/Misc/GeneralFunctions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+namespace ANamelessHero
+{
+
+    internal static class GeneralFunctions
+    {
+        public static GraphicsDeviceManager Graphics;
+        public static ContentManager Content;
+        public static GameTime GameTime;
+        public static GraphicsDevice GraphicsDevice;
+        public static void Init(GraphicsDeviceManager _graphics, ContentManager _content, GraphicsDevice _graphicsDevice)
+        {
+            Graphics = _graphics;
+            Content = _content;
+            GraphicsDevice = _graphicsDevice;
+        }
+    }
+
+}
